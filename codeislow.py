245b19b72468b2f2ed97b8e6eba6f05520db6474
c24b Constance de Quatrebarbes <4barbes@gmail.com>
2022-10-16 01:15:50 +0200

    [FIX] Credential CLIENT_ID is CLIENT_KEY in dotenv file

--- codeislow.py
+++ codeislow.py
@@ -32,7 +32,7 @@ def legifrance_auth():
     token_url = "https://oauth.piste.gouv.fr/api/oauth/token"
 
     
-    client_id = os.environ.get("CLIENT_ID")
+    client_id = os.environ.get("CLIENT_KEY")
     client_secret = os.environ.get("CLIENT_SECRET")
 
     res = requests.post(
