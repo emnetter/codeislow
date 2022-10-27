6007af10dd1b173e3488bf4c8fbf907e03f848f0
c24b <4barbes@gmail.com>
2022-10-27 21:10:29 +0200

    [TESTS] test content

--- tests/test_parsing_001.py
+++ tests/test_parsing_001.py
@@ -10,6 +10,15 @@ from odf.opendocument import load
 ACCEPTED_EXTENSIONS = ("odt", "pdf", "docx", "doc")
 
 def parse_doc(file_path):
+    """
+    Parsing document
+    Arguments:
+        file_path: a string representing the filepath of the document
+    Returns:
+        full_text: a list of sentences
+    Raises:
+        FileNotFoundError: File has not been found. file_path must be incorrect
+    """
     doc_name, doc_ext = file_path.split("/")[-1].split(".")
     if doc_ext not in ACCEPTED_EXTENSIONS:
         raise Exception("Extension incorrecte: les fichiers acceptés terminent par *.odt, *docx, *.pdf")
@@ -18,16 +27,16 @@ def parse_doc(file_path):
     if doc_ext == "pdf":
         with open(file_path, "rb") as f:    
             reader = PdfReader(f)
-            paragraphs = reader.pages
-            for i in range(len(paragraphs)):
+            
+            for i in range(len(reader.pages)):
                 page = reader.pages[i]
-                full_text.append((page.extract_text()))
+                full_text.extend((page.extract_text()).split("\n"))
             
     elif doc_ext == "odt":
         with open(file_path, "rb") as f:
             document = load(f)
-            paragraphs = document.getElementsByType(text.P)
-            for i in range(len(paragraphs)):
+            paragraphs = len(document.getElementsByType(text.P))
+            for i in range(paragraphs):
                 full_text.append((teletype.extractText(paragraphs[i])))
     else:
         # if doc_ext in ["docx", "doc"]:
@@ -36,7 +45,8 @@ def parse_doc(file_path):
             paragraphs = document.paragraphs
             for i in range(len(paragraphs)):
                 full_text.append((paragraphs[i].text))
-    return (len(paragraphs), full_text)
+    full_text = [n for n in full_text if n not in  ["\n", "", " "]]
+    return full_text
     
 class TestFileUpload:
     def test_wrong_extension(self):
@@ -52,17 +62,18 @@ class TestFileUpload:
         with pytest.raises(FileNotFoundError):
             parse_doc(filepath)
 
-    def test_read_file(self):
-        '''test output'''
-        file_paths = ["./newtest.doc", "./newtest.docx", "./newtest.pdf"]
+    def test_content(self):
+        '''test content'''
+        file_paths = ["newtest.doc", "newtest.docx", "newtest.pdf"]
         for file_path in file_paths:
             abspath = os.path.join(os.path.dirname(os.path.realpath(__file__)), file_path)
-            nb_paragraph, full_text = parse_doc(abspath)
+            full_text = parse_doc(abspath)
             doc_name, doc_ext = abspath.split("/")[-1].split(".")
-            assert doc_name == newtest
+            assert doc_name == "newtest"
             if abspath.endswith(".pdf"):
-                assert nb_paragraph == 1
+                assert len(full_text) == 21, (len(full_text), abspath)
             else:
-                assert nb_paragraph == 42, (nb_paragraph, abspath)
+                assert len(full_text) == 22, (len(full_text), abspath)
             assert any("art." in _x for _x in full_text) is True
             assert any("Art." in _x for _x in full_text) is True
+            
