35ee51cf34b3cad55acc890e1613cf58b391a82f
c24b <4barbes@gmail.com>
2022-10-31 13:57:36 +0100

    M formatting

--- tests/test_check_004.py
+++ tests/test_check_004.py
@@ -5,7 +5,13 @@ from dotenv import load_dotenv
 
 import pytest
 
-from test_request_003 import MAIN_CODELIST, get_legifrance_auth, get_article_uid,  get_article_content
+from test_request_003 import (
+    MAIN_CODELIST,
+    get_legifrance_auth,
+    get_article_uid,
+    get_article_content,
+)
+
 
 def check_validity(years_before, years_after, dateDebut, dateFin):
     """
@@ -30,54 +36,108 @@ def check_validity(years_before, years_after, dateDebut, dateFin):
 def get_article(code_name, article_number, client_id, client_secret):
     """
     Accéder aux informations de status de l'article
-    
+
     Arguments:
         code_name: Nom du code de loi française dans sa version longue
         article_number: NUméro de l'article de loi normalisé ex. R25-67 L214 ou 2667-1-1
     Returns:
-        article: Un dictionnaire json avec le texte les dates de début et de fin, l'url, les versions de l'article   
-    Raise: 
+        article: Un dictionnaire json avec le texte les dates de début et de fin, l'url, les versions de l'article
+    Raise:
         Exception(Indisponible): L'article n'a pas été trouvé
         ValueError(Code indisponible): Le nom du code est incorrect/ n'a pas été trouvé
     """
     article = {
         "code_full_name": MAIN_CODELIST[code_name],
         "code_short_name": code_name,
-        "number": article_number
+        "number": article_number,
+        "status_code": 200,
+        "status": "",
     }
-    article_id = get_article_uid(code_name, article_number, headers=get_legifrance_auth(client_id, client_secret))
+    article_id = get_article_uid(
+        code_name, article_number, headers=get_legifrance_auth(client_id, client_secret)
+    )
     if article_id is None:
         article["status_code"] = 404
         article["status"] = "Indisponible"
         article["id"] = None
         return article
-    article_content = get_article_content(article_id, headers=get_legifrance_auth(client_id, client_secret))
+    article_content = get_article_content(
+        article_id, headers=get_legifrance_auth(client_id, client_secret)
+    )
     article.update(article_content)
     return article
+
 
 class TestGetArticle:
     def test_get_single_article(self):
         load_dotenv()
         client_id = os.getenv("API_KEY")
         client_secret = os.getenv("API_SECRET")
-        article = get_article("CCONSO","L121-14", client_id, client_secret)
+        article = get_article("CCONSO", "L121-14", client_id, client_secret)
         assert article["dateDebut"] == 1467331200000, article["dateDebut"]
         assert article["dateFin"] == 32472144000000, article["dateFin"]
-        assert article["url"] == "https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000032227262", article["url"]
-        assert article["texte"] == "Le paiement résultant d'une obligation législative ou réglementaire n'exige pas d'engagement exprès et préalable."
-        assert article["code_full_name"] == "Code de la consommation", article["code_full_name"]
+        assert (
+            article["url"]
+            == "https://www.legifrance.gouv.fr/codes/article_lc/LEGIARTI000032227262"
+        ), article["url"]
+        assert (
+            article["texte"]
+            == "Le paiement résultant d'une obligation législative ou réglementaire n'exige pas d'engagement exprès et préalable."
+        )
+        assert article["code_full_name"] == "Code de la consommation", article[
+            "code_full_name"
+        ]
         assert article["code_short_name"] == "CCONSO", article["code_short_name"]
         assert article["nb_versions"] == 1, article["nb_versions"]
 
-    @pytest.mark.parametrize("input_expected", [("CCONSO","L121-14", 'LEGIARTI000032227262'), ("CCONSO","R742-52","LEGIARTI000032808914"), ("CSI","L622-7","LEGIARTI000043540586"), ("CSI","R314-7","LEGIARTI000037144520"), 
-    ("CGCT","L1424-71", "LEGIARTI000028529379"), ("CJA", "L121-2", "LEGIARTI000043632528"), ("CESEDA", "L753-1", "LEGIARTI000042774802"), ("CENV", "L124-1", "LEGIARTI000033140333")])
-    def test_get_articles(self, input_expected):
+    @pytest.mark.parametrize(
+        "input_expected",
+        [
+            ("CCONSO", "121-14", "LEGIARTI000032227262"),
+            ("CCONSO", "R742-52", "LEGIARTI000032808914"),
+            ("CSI", "L622-7", "LEGIARTI000043540586"),
+            ("CSI", "R314-7", "LEGIARTI000037144520"),
+            ("CGCT", "L1424-71", "LEGIARTI000028529379"),
+            ("CJA", "L121-2", "LEGIARTI000043632528"),
+            ("CESEDA", "L753-1", "LEGIARTI000042774802"),
+            ("CENV", "L124-1", "LEGIARTI000033140333"),
+        ],
+    )
+    def test_get_multiple_articles(self, input_expected):
         load_dotenv()
         client_id = os.getenv("API_KEY")
         client_secret = os.getenv("API_SECRET")
         code_short_name, art_num, art_id = input_expected
-        article = get_article(code_short_name,art_num, client_id, client_secret)
+        article = get_article(code_short_name, art_num, client_id, client_secret)
         assert article["id"] == art_id, (code_short_name, art_num, article["id"])
         assert article["code_short_name"] == code_short_name, article["code_short_name"]
-        assert article["code_full_name"] == MAIN_CODELIST[code_short_name], article["code_full_name"]
+        assert article["code_full_name"] == MAIN_CODELIST[code_short_name], article[
+            "code_full_name"
+        ]
+        assert article["status_code"] == 200
 
+    @pytest.mark.parametrize(
+        "input_expected",
+        [
+            ("CCONSO", "R121-14", None),
+            ("CCONSO", "742-52", None),
+            ("CSI", "622-7", None),
+            ("CSI", "314-7", None),
+            ("CGCT", "1424-71", None),
+            ("CJA", "121-2", None),
+            ("CESEDA", "753-1", None),
+            ("CENV", "124-1", None),
+        ],
+    )
+    def test_get_not_found_articles(self, input_expected):
+        load_dotenv()
+        client_id = os.getenv("API_KEY")
+        client_secret = os.getenv("API_SECRET")
+        code_short_name, art_num, art_id = input_expected
+        article = get_article(code_short_name, art_num, client_id, client_secret)
+        assert article["id"] == art_id, (code_short_name, art_num, article["id"])
+        assert article["code_short_name"] == code_short_name, article["code_short_name"]
+        assert article["code_full_name"] == MAIN_CODELIST[code_short_name], article[
+            "code_full_name"
+        ]
+        assert article["status_code"] == 404
