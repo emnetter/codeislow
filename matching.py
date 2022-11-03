047e67a61aa9c61b4ec742f5de7108a02ee694d4
c24b <4barbes@gmail.com>
2022-11-03 20:53:16 +0100

    [APP] modules with FULL documentation

--- matching.py
+++ matching.py
@@ -1,74 +1,75 @@
-import logging
-import os
+#!/usr/bin/env python3
+#filename: matching.py
+"""
+The matching module
+
+Ce module permet la detection des articles du code de droit français
+
+"""
+
 import re
-import pytest
 
 from parsing import parse_doc
-from requesting import MAIN_CODELIST
-
-# logging.basicConfig(filename="matching.log", encoding="utf-8", level=logging.DEBUG)
+from code_references import filter_code_regex, CODE_REFERENCE, CODE_REGEX
 
 ARTICLE_REGEX = r"(?P<art>(Articles?|Art\.))"
-CODE_DICT = {
-    "CCIV": r"(?P<CCIV>Code civil|C\.\sciv\.|Code\sciv\.|C\.civ\.|civ\.|CCIV)",
-    "CPRCIV": r"(?P<CPRCIV>Code\sde\sprocédure civile|C\.\spr\.\sciv\.|CPC)",
-    "CCOM": r"(?P<CCOM>Code\sde\scommerce|C\.\scom\.)",
-    "CTRAV": r"(?P<CTRAV>Code\sdu\stravail|C\.\strav\.)",
-    "CPI": r"(?P<CPI>Code\sde\sla\spropriété\sintellectuelle|CPI|C\.\spr\.\sint\.)",
-    "CPEN": r"(?P<CPEN>Code\spénal|C\.\spén\.)",
-    "CPP": r"(?P<CPP>Code\sde\sprocédure\spénale|CPP)",
-    "CASSU": r"(?P<CASSUR>Code\sdes\sassurances|C\.\sassur\.)",
-    "CCONSO": r"(?P<CCONSO>Code\sde\sla\sconsommation|C\.\sconso\.)",
-    "CSI": r"(?P<CSI>Code\sde\slasécurité intérieure|CSI)",
-    "CSP": r"(?P<CSP>Code\sde\slasanté publique|C\.\ssant\.\spub\.|CSP)",
-    "CSS": r"(?P<CSS>Code\sde\slasécurité sociale|C\.\ssec\.\ssoc\.|CSS)",
-    "CESEDA": r"(?P<CESEDA>Code\sde\sl'entrée\set\sdu\sséjour\sdes\sétrangers\set\sdu\sdroit\sd'asile|CESEDA)",
-    "CGCT": r"(?P<CGCT>Code\sgénéral\sdes\scollectivités\sterritoriales|CGCT)",
-    "CPCE": r"(?P<CPCE>Code\sdes\spostes\set\sdes\scommunications\sélectroniques|CPCE)",
-    "CENV": r"(?P<CENV>Code\sde\sl'environnement|C.\senvir.|\sCE\.?\s\.?)",
-    "CJA": r"(?P<CJA>Code\sde\sjustice\sadministrative|CJA)",
-}
 
-CODE_REGEX = "|".join(CODE_DICT.values())
 # ARTICLE_REF = re.compile("\d+")
-# ARTICLE_ID = re.compile("?((L|R|A|D)?(\.))(\d+)?(-\d+)?(\s(al\.|alinea)\s\d+)")
+# ARTICLE_ID = r"(L|R|A|D)?(\.|\s)?\d+(-\d+)?((\s(al\.|alinea)?\s\d+)?(\s|\.)"
 
 
-def switch_pattern(pattern="article_code"):
+
+
+def switch_pattern(selected_codes=None, pattern="article_code"):
     """
     Build pattern recognition using pattern short code switch
 
-    Arguments:
-        pattern: a string article_code or code_article
-
-    Returns:
-        regex_pattern: compiled regex pattern
-    Raise:
-        ValueError: pattern name is wrong
+    Arguments
+    ---------
+    selected_codes: array
+        a list of short codes to select. Default to None
+    pattern: str
+        a string article_code or code_article. Default to article_code
+    Returns
+    ---------
+    regex_pattern: str
+        a compiled regex pattern
+    Raise
+    --------
+    ValueError: 
+        pattern name is wrong
     """
+
+    code_regex = filter_code_regex(selected_codes)
 
     if pattern not in ["article_code", "code_article"]:
         raise ValueError(
             "Wrong pattern name: choose between 'article_code' or 'code_article'"
         )
     if pattern == "article_code":
-        return re.compile(f"{ARTICLE_REGEX}(?P<ref>.*?)({CODE_REGEX})", flags=re.I)
-    else:
-        return re.compile(f"({CODE_REGEX}){ARTICLE_REGEX}(?P<ref>.*?)$)", flags=re.I)
+        return re.compile(f"{ARTICLE_REGEX}(?P<ref>.*?){code_regex}", flags=re.I)
+    # else:
+    #     #code_article
+    #     # return re.compile(f"{code_regex}.*?{ARTICLE_REGEX}(\s|\.)(?P<ref>.*?)(\.|\s)", flags=re.I)
+    #     return re.compile(f"{code_regex}.*?{ARTICLE_REGEX}.*?{ARTICLE_ID}", flags=re.I)
 
-
-def match_code_and_articles(full_text, pattern_format="article_code"):
-    """ "
-    Detect law articles of supported codes
-
-    Arguments:
-        full_text: a string of the full document normalized
-        pattern_format: a string representing the pattern format article_code or code_article. Defaut to article_code
-
-    Returns:
-        code_found: a dict compose of short version of code as key and list of the detected articles references  as values {code: [art_ref, art_ref2, ... ]}
+def get_matching_results_dict(full_text, selected_short_codes=[], pattern_format="article_code"):
     """
-    article_pattern = switch_pattern(pattern_format)
+    Une fonction qui renvoie un dictionnaire de resultats: trié par code (version abbréviée) avec la liste des articles détectés lui appartenant. 
+
+    Arguments
+    ----------
+    full_text: str
+        a string of the full document normalized
+    pattern_format: str
+        a string representing the pattern format article_code or code_article. Defaut to article_code
+
+    Returns
+    ----------
+    code_found: dict
+        a dict compose of short version of code as key and list of the detected articles references  as values {code: [art_ref, art_ref2, ... ]}
+    """
+    article_pattern = switch_pattern(selected_short_codes, pattern_format)
     code_found = {}
 
     # normalisation
@@ -126,24 +127,28 @@ def match_code_and_articles(full_text, pattern_format="article_code"):
 
     return code_found
 
-def gen_matching_results(full_text, pattern_format="article_code"):
-    """ "
-    Detect law articles of supported codes
+def get_matching_result_item(full_text, selected_shortcodes=[], pattern_format="article_code"):
+    """"
+    Renvoie les références des articles détectés dans le texte
 
-    Arguments:
-        full_text: a string of the full document normalized
-        pattern_format: a string representing the pattern format article_code or code_article. Defaut to article_code
+    Arguments
+    -----------
+    full_text: str
+        a string of the full document normalized
+    selected_shortcodes: array
+        a list of selected codes in short format for filtering article detection. Default is an empty list (which stands for no filter) 
+    pattern_format: str
+    a string representing the pattern format article_code or code_article. Defaut to article_code
 
-    Yield:
-        code_short_name
-        code_long_name
-        article
+    Yields
+    --------
+    code_short_name:str
+
+    article_number:str
     """
-    article_pattern = switch_pattern(pattern_format)
-    code_found = {}
-
-    # normalisation
-    full_text = re.sub(r"\r|\n|\t|\xa0", " ", " ".join(full_text))
+    article_pattern = switch_pattern(selected_shortcodes, pattern_format)
+    # normalisation des espaces dans le texte
+    full_text = re.sub(r"\r|\n|\t|\f|\xa0", " ", " ".join(full_text))
     for i, match in enumerate(re.finditer(article_pattern, full_text)):
         needle = match.groupdict()
         qualified_needle = {
@@ -155,7 +160,7 @@ def gen_matching_results(full_text, pattern_format="article_code"):
         code = [k for k in qualified_needle.keys() if k not in ["ref", "art"]][0]
 
         ref = match.group("ref").strip()
-        # split multiple articles of a same code
+        # split multiple articles of a same code example: Article 22, 23 et 24 du Code
         refs = [
             n
             for n in re.split(r"(\set\s|,\s|\sdu)", ref)
@@ -173,20 +178,19 @@ def gen_matching_results(full_text, pattern_format="article_code"):
             for ref in refs
         ]
         # clean caracters for everything but numbers and (L|A|R|D) and caret
-        normalized_refs = []
         for ref in refs:
             # accepted caracters for article
+            # exemple: 1224 du => Non 298 al 32 => Non R-288 => oui A-24-14=> oui
             ref = "".join(
                 [n for n in ref if (n.isdigit() or n in ["L", "A", "R", "D", "-"])]
             )
             if ref.endswith("-"):
                 ref = ref[:-1]
             # remove caret separating article nb between first letter
+            # exemple: L-248-1 = > L248-1
             special_ref = ref.split("-", 1)
             if special_ref[0] in ["L", "A", "R", "D"]:
-                # normalized_refs.append("".join(special_ref))
-                yield(code, MAIN_CODELIST[code], "".join(special_ref))
+                yield(code, "".join(special_ref))
+                
             else:
-                # normalized_refs.append(ref)
-                yield(code, MAIN_CODELIST[code], ref)
-
+                yield(code, ref)
